192f88fb444a97b604be50fd32ef7120c8db30e0
Jonathan <Jonathan.dyrekilde.sommerlund@gmail.com>
2024-02-17 17:17:23 +0100

    List of machines at random positions

--- Solver.py
+++ Solver.py
@@ -1,5 +1,17 @@
 from factoriocalc import *
 from factoriocalc.presets import *
+import random
+
+class LocatedMachine:
+    position = None
+    machine = None
+    def __init__(self, machine, position):
+        self.machine = machine
+        self.position = position
+
+    def __str__(self) -> str:
+        return  str(self.machine) + " at " + str(self.position)
+
 
 # Input for the blueprint
 inputItems = [itm.iron_plate, itm.copper_plate]
@@ -14,4 +26,15 @@ config.machinePrefs.set(MP_LATE_GAME)
 config.machinePrefs.set([mch.AssemblingMachine2()])
 
 factory = produce([desiredOutput @ throughput], using = inputItems).factory
-print(factory.summary())
+
+machineCounts = factory.inner.machine.machines
+locatedMachines = []
+
+for machine in machineCounts:
+    for i in range(machine.num):
+        pos = [random.random(), random.random()]
+        locatedMachine = LocatedMachine(machine.machine, pos)
+        locatedMachines.append(locatedMachine)
+
+for locatedMachine in locatedMachines:
+    print(locatedMachine)
