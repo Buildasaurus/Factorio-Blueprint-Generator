4aa0a6be60d2fad2a410001df9df5b5a418564bb
Buildasaurus <jonathan.dyrekilde.sommerlund@gmail.com>
2024-05-18 15:55:56 +0200

    Update direction test

--- test/astar/direction.py
+++ test/astar/direction.py
@@ -1,6 +1,6 @@
-'''
+"""
     Testing if methods are correctly implemented
-'''
+"""
 
 import logging
 import unittest
@@ -36,29 +36,39 @@ log.info("unittest of underground belts")
 #  Game constants
 #
 
-WOOD_CHEST = 'wooden-chest'
-IRON_CHEST = 'iron-chest'
-BELT = 'transport-belt'
+WOOD_CHEST = "wooden-chest"
+IRON_CHEST = "iron-chest"
+BELT = "transport-belt"
 
 
 #
 #  Test
 #
 
+
 class Test_find_underground_entry(unittest.TestCase):
-    '''Cases where it would be impossible without an underground belt'''
+    """Cases where it would be impossible without an underground belt"""
+
     def test_underground_method(self):
-        '''
+        """
         A main belt may be so wide that you need to surface to cross it
         in this configuration:
         0 1 2 3 4 5 6
         e x x x x x o
         the output should be the node at index 1
-        '''
-        log.debug(f'test')
+        """
+        log.debug(f"test")
 
-        site = layout.ConstructionSite(50,50)
-        finder = A_star(site,[(0,0)],[(1,1)])
-        node = finder.find_entrance_node(finder.nodes[0][0], finder.nodes[0][6])
-        log.debug(f'Node at: ')
-        self.assertEqual(node.position, (1,0))
+        site = layout.ConstructionSite(50, 50)
+        finder = A_star(site, [(0, 0)], [(1, 1)])
+        start_position = (5, 5)
+        end_positions = [(3, 5), (7, 5), (5, 3), (5, 7)]
+        correct_directions = [(4, 5), (6, 5), (5,4), (5,6)]
+        counter = 0
+        for end in end_positions:
+            node = finder.find_entrance_node(
+                finder.nodes[start_position[1]][start_position[0]],
+                finder.nodes[end[1]][end[0]],
+            )
+            self.assertEqual(node.position, correct_directions[counter])
+            counter += 1
