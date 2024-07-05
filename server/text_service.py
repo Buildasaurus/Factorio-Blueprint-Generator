6c20a9b5a3da89347146174ca84cb79b3c921509
Buildasaurus <79661189+Buildasaurus@users.noreply.github.com>
2024-07-05 14:46:45 +0200

    Update server and add service

---
+++ server/text_service.py
@@ -0,0 +1,11 @@
+from concurrent import futures
+import numpy as np
+from grpc_generated import service_pb2_grpc
+from grpc_generated import service_pb2
+
+class TextService(service_pb2_grpc.TextService):
+    '''Expecting request to be a string'''
+    def DoText(self, request, context):
+        print(f"Received {request} as string")
+        result = request + " added text!"
+        return service_pb2.TextThing(text=result)
