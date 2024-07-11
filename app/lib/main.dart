c36605db36d15413c67d78f99243c0d0c97f022a
Buildasaurus <79661189+Buildasaurus@users.noreply.github.com>
2024-07-11 09:26:44 +0200

    Update gitignore to include lib
    Since this is where the main dart file is located.

---
+++ app/lib/main.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Flutter HTTP Example')),
+        body: const Center(
+          child: MyHomePage(),
+        ),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String _response = '';
+
+  Future<void> _sendRequest(String request) async {
+    final response = await http.post(
+      Uri.parse('http://localhost:5000/process'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        'input_string': request,
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      setState(() {
+        _response = jsonDecode(response.body)['output_string'];
+      });
+    } else {
+      setState(() {
+        _response = 'Error: ${response.statusCode}';
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        ElevatedButton(
+          onPressed: () => _sendRequest("Generate electric circuit"),
+          child: const Text('Generate electric circuit'),
+        ),
+        const SizedBox(height: 10),
+        ElevatedButton(
+          onPressed: () => _sendRequest("Generate mall"),
+          child: const Text('Generate mall'),
+        ),
+        const SizedBox(height: 20),
+        SelectableText('Blueprint generator response: $_response'),
+      ],
+    );
+  }
+}
