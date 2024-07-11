66e34061714aedd935f09d6aa1bd17ab5d2c41f2
Buildasaurus <79661189+Buildasaurus@users.noreply.github.com>
2024-07-11 09:14:48 +0200

    Update server logging

--- server/server.py
+++ server/server.py
@@ -8,36 +8,27 @@ import solver
 app = Flask(__name__)
 
 # Set up logging
-logging.basicConfig(filename='myapp.log', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(filename='server.log', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger()
-
-# Initialize game configuration
-def initialize_game():
-    logger.debug('Initializing game configuration')
-    fc.config.machinePrefs.set(fcc.MP_LATE_GAME)
-    fc.config.machinePrefs.set([fc.mch.AssemblingMachine2()])
-    logger.debug('Game configuration initialized successfully')
-
-initialize_game()
 
 def GenerateBlueprint(blueprint_input):
     '''This is copied from the mall_small test'''
-    def writeText(text):
-        with open("text_service.log", "a") as f:
-            f.write(str(text) + "\n")
-
     try:
-        writeText("Initializing blueprint generation")
+        logger.info('Starting blueprint generation process')
+        logger.debug('Initializing game configuration')
+
+        fc.config.machinePrefs.set(fcc.MP_LATE_GAME)
+        fc.config.machinePrefs.set([fc.mch.AssemblingMachine2()])
+
         WIDTH = 64  # blueprint width
         HEIGHT = 64  # blueprint height
 
-        # Input for the blueprint
         input_items = [fc.itm.iron_plate, fc.itm.copper_plate]
-
-        # Output for the blueprint
         desired_output = fc.itm.electronic_circuit
-
         throughput = 3  # items per second. 3 is equivalent to 2 green circuits, 3 copper assembling 2 machines
+
+        logger.debug('Game configuration initialized successfully')
+        logger.debug('Generating blueprint')
 
         # Machines for construction - assembly types & smelting type
         factory = fc.produce(
@@ -47,18 +38,24 @@ def GenerateBlueprint(blueprint_input):
         site = layout.ConstructionSite(WIDTH, HEIGHT)
         machines = solver.randomly_placed_machines(factory, site.size())
         solver.add_connections(machines)
+
         solver.spring(machines, borders=((0, 0), (WIDTH, HEIGHT)))
-        writeText("Machines are at: " + str([machine.position for machine in machines]))
+        logger.debug("Machines are at: " + str([machine.position for machine in machines]))
+
         solver.machines_to_int(machines)
-        writeText("Machines are at: " + str([machine.position for machine in machines]))
+        logger.debug("Machines as integers are at: " + str([machine.position for machine in machines]))
+
+        logger.debug("Placing on site")
         solver.place_on_site(site, machines, None)
-        writeText(str(site))
+        logger.debug(str(site))
+
         blueprint_string = layout.site_as_blueprint_string(site, label="test of blueprint code")
-        writeText(blueprint_string)
-        writeText("End of test")
+        logger.debug(f"Generated following blueprint string: {blueprint_string}")
+        logger.info('Completed blueprint generation process')
+
         return f"Blueprint generation complete: {blueprint_string}"
     except Exception as e:
-        writeText(e)
+        logger.error(e)
         return f"failed to {e}"
 
 @app.route('/process', methods=['POST'])
