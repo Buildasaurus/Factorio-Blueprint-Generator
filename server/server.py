a8b6317598f77bf5dedf3795f508f3c60dbfd522
Peer Sommerlund <peer.sommerlund@gmail.com>
2024-07-19 05:06:20 +0200

    Enable Cross-Origin Resource Sharing

--- server/server.py
+++ server/server.py
@@ -1,6 +1,8 @@
 import logging
 
 from flask import request, jsonify
+from connexion.middleware import MiddlewarePosition
+from starlette.middleware.cors import CORSMiddleware
 import connexion
 import factoriocalc as fc
 import factoriocalc.presets as fcc
@@ -14,6 +16,16 @@ logger = logging.getLogger()
 
 # Initialize server
 app = connexion.FlaskApp(__name__)
+    
+app.add_middleware(
+    CORSMiddleware,
+    position=MiddlewarePosition.BEFORE_EXCEPTION,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.add_api('fbg-api.yaml')
 
 
